dc7656c468a02d5b8481e7427c8a5ec5c836a32b
Hasmitha Tadavarthy <tadavarthyhasmitha@gmail.com>
2026-06-24 23:55:32 +0530

    Initial commit: Sam Robot autonomous navigation package

---
+++ sam_gazebo/launch/world.launch.py
@@ -0,0 +1,52 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    # Declare the world file argument
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='home.sdf',
+        description='Name of the Gazebo world file to load'
+    )
+
+    # Get package directories
+    pkg_sam_gazebo = get_package_share_directory('sam_gazebo')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    # Update GZ_SIM_RESOURCE_PATH and IGN_GAZEBO_RESOURCE_PATH with the path to worlds folder
+    gazebo_models_path = os.path.join(pkg_sam_gazebo, 'worlds')
+    for env_var in ["GZ_SIM_RESOURCE_PATH", "IGN_GAZEBO_RESOURCE_PATH"]:
+        if env_var in os.environ:
+            os.environ[env_var] += os.pathsep + gazebo_models_path
+        else:
+            os.environ[env_var] = gazebo_models_path
+
+    # Include Gazebo launch file
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
+        ),
+        launch_arguments={
+            'gz_args': [PathJoinSubstitution([
+                pkg_sam_gazebo,
+                'worlds',
+                LaunchConfiguration('world')
+            ]),
+            TextSubstitution(text=' -r -v -v1')],
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
+
+    # Create launch description
+    launchDescriptionObject = LaunchDescription()
+
+    # Add actions
+    launchDescriptionObject.add_action(world_arg)
+    launchDescriptionObject.add_action(gazebo_launch)
+
+    return launchDescriptionObject
